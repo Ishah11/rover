d954e716ecbe0a17ba0dab1acb8693c530295f28
Ishah11 <165867047+Ishah11@users.noreply.github.com>
2024-04-10 04:03:27 +0100

    Update trialData.py

--- trialData.py
+++ trialData.py
@@ -6,34 +6,28 @@ import smbus2
 import sys
 
 #import rx
-from rx import operators as ops
 from influxdb_client import InfluxDBClient, Point, WritePrecision, WriteOptions
 from influxdb_client.client.write_api import SYNCHRONOUS
 
 # Configure InfluxDB connection variables
-token = "<InfluxDB User Token>"
-org = "<InfluxDB Org>"
-bucket = "<InfluxDB Bucket>"
-_client = InfluxDBClient(url="<InfluxDB URL>", token=token)
-_write_client = _client.write_api(write_options=WriteOptions(batch_size=500,
-                                                             flush_interval=10_000,
-                                                             jitter_interval=2_000,
-                                                             retry_interval=5_000,
-                                                             max_retries=5,
-                                                             max_retry_delay=30_000,
-                                                             exponential_base=2))
+token = "nloTTzGPJrlM_a9MwAIGz789f5Y2tEGci5GUYfRskb1GCCRTLBbCXCRHXapQk5cHu3aYAFg1E0Qf2ysY1riTmw=="
+org = "rover"
+bucket = "pi-rover"
+_client = InfluxDBClient(url="http://localhost:8086/", token=token) 
+#change client to 192.168.1.5 inplace of local host for any other pis over the rover network
+_write_client = _client.write_api()
 
 #Collect data
-ser = serial.Serial('/dev/ttyUSB0')
-port = 1
-address = 0x77
-bus = smbus2.SMBus(port)
+# ser = serial.Serial('/dev/ttyUSB0')
+# port = 1
+# address = 0x77
+# bus = smbus2.SMBus(port)
 
 #bme280.load_calibration_params(bus,address)
 
 #Configure the labels for the database
 measurement = "rpi-bme280"
-location = "<location>"
+location = "rpi"
 
 try:
     while True:
@@ -41,7 +35,7 @@ try:
         timestamp_aq = datetime.timestamp(now)
         iso = datetime.utcnow()
         data = []
-        file1 = open('sampleData.txt', 'r')
+        file1 = open('/home/pi/Documents/sampleData.txt', 'r')
         Lines = file1.readlines()
  
     # Strips the newline character
@@ -57,13 +51,13 @@ try:
             #humidity = bme280_data.humidity
             
            # Print for debugging, uncomment the below line
-            #print("[%s] Temp: %s, Humidity: %s, Pressure: %s, Air_Quality_pmtwofive: %s, Air_Quality_pmten: %s" % (iso, temperature, humidity, pressure, pmtwofive, pmten))
 
             find = line.split(", ")
             temperature = float(find[2])
             humidity = float(find[1])
             pressure = float(find[3])
             pmten = float(find[4])
+            print("[%s] Temp: %s, Humidity: %s, Pressure: %s,Air_Quality_pmten: %s" % (iso, temperature, humidity, pressure, pmten))
 
             _write_client.write(bucket, org, [{"measurement": measurement, "tags": {"location": location},
                                               "fields": {"temperature": temperature}, "time": iso}])
@@ -73,7 +67,6 @@ try:
                                               "fields":{"air-pressure": pressure}, "time": iso}])
             _write_client.write(bucket, org, [{"measurement":measurement, "tags": {"location": location},
                                               "fields":{"aq-pmten": pmten}, "time": iso}])
-        time.sleep(10)
 
 except KeyboardInterrupt:
     _write_client.__del__()
